51f0071e6edda0bf873b75a3739c1d5166b46e46
SeaNot-Not <seancortez41@gmail.com>
2025-01-29 15:49:46 +0800

    init: create initial files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-gray-900 flex justify-center items-center">
+      <p className="text-5xl font-bold text-white">Kulinarya Frontend</p>
+    </div>
+  );
+}
+
+export default App;
