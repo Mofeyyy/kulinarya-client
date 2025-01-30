fe4fe62107d5a9415bc24205a148bd901f5d5436
SeaNot-Not <seancortez41@gmail.com>
2025-01-30 12:42:23 +0800

    feat: connect to mongoDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  // Test DB Connection
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/")
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(`Error: ${error}`));
+  }, []);
+
   return (
     <div className="h-screen w-screen bg-gray-900 flex justify-center items-center">
       <p className="text-5xl font-bold text-white">Kulinarya Frontend</p>
