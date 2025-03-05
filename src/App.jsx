538de9cd2441172b417fbac7922fdcaecff22ba3
SeaNot-Not <seancortez41@gmail.com>
2025-03-05 14:28:09 +0800

    feat: install react-hot-toast and implement it in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useEffect } from "react";
 import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   // Test DB Connection
   useEffect(() => {
     axios
       .get("http://localhost:4000/")
-      .then((response) => console.log(response.data))
+      .then((response) => {
+        if (response.status === 201) {
+          toast.success(response.data.message);
+        }
+        console.log(response.data);
+      })
       .catch((error) => console.log(`Error: ${error}`));
   }, []);
 
   return (
     <div className="h-screen w-screen bg-gray-900 flex justify-center items-center">
+      <Toaster />
+
       <p className="text-5xl font-bold text-white">Kulinarya Frontend</p>
     </div>
   );
