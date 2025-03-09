e91c27ba4c25d0a9fe5af9d9d0b137c4fa86f2ff
SeaNot-Not <seancortez41@gmail.com>
2025-03-09 12:39:19 +0800

    refactor: login form

--- src/forms/useSignupForm.js
+++ src/forms/useSignupForm.js
@@ -9,12 +9,7 @@ const useSignupForm = () => {
   const { login } = useAuthStore();
   const navigateTo = useNavigate();
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isSubmitSuccessful, isSubmitting },
-  } = useForm({
+  const loginForm = useForm({
     defaultValues: {
       email: "",
       password: "",
@@ -23,9 +18,18 @@ const useSignupForm = () => {
     resolver: zodResolver(loginSchema),
   });
 
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { isSubmitSuccessful, isSubmitting },
+  } = loginForm;
+
   const onSubmit = async (data) => {
     console.log(data);
-    const result = await login(data.email, data.password);
+
+    const { email, password } = data;
+    const result = await login(email, password);
 
     if (result.success) {
       toast.success("Login Success!");
@@ -37,30 +41,13 @@ const useSignupForm = () => {
     document.querySelector("#emailInput").focus();
   };
 
-  // For code readability
-  // Note: the "!!" or double negation converts to its boolean value to ensure that the value is boolean
-  // Note: the "?" or optional chaining prevents runtime error if the property that we will access is null or undefined
-  const isEmailHasError = !!errors.email;
-  const isPassHasError = !!errors.password;
-
-  const emailErrorMessage = errors.email?.message;
-  const passErrorMessage = errors.password?.message;
-
-  // Array object for rendering
-  const errorMessages = [
-    { hasError: isEmailHasError, message: emailErrorMessage },
-    { hasError: isPassHasError, message: passErrorMessage },
-  ];
-
   return {
-    register,
+    loginForm,
+    control,
     handleSubmit,
     isSubmitSuccessful,
     isSubmitting,
     onSubmit,
-    isEmailHasError,
-    isPassHasError,
-    errorMessages,
   };
 };
 
--- src/pages/auth/components/LoginForm.jsx
+++ src/pages/auth/components/LoginForm.jsx
@@ -1,75 +1,102 @@
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import useAuthStore from "@/stores/useAuthStore";
-import useSignupForm from "@/forms/useSignupForm";
-import { LoaderCircle } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+
+// Imported Icons
+import { LoaderCircle } from "lucide-react";
+
+// Imported Components
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+// Imported Stores
+import useAuthStore from "@/stores/useAuthStore";
+
+// Imported Forms
+import useSignupForm from "@/forms/useSignupForm";
+
+// ----------------------------------------------------------------
 
 export function LoginForm({ className, ...props }) {
   const { isLoading, apiError } = useAuthStore();
   const navigateTo = useNavigate();
 
   const {
-    register,
+    loginForm,
+    control,
     handleSubmit,
     isSubmitSuccessful,
     isSubmitting,
     onSubmit,
-    isEmailHasError,
-    isPassHasError,
-    errorMessages,
   } = useSignupForm();
 
-  //Finding error to the errorMessages array, the first error founded will be stored to the renderError variable, if no error, it will become undefined
-  const renderError = errorMessages.find((error) => error.hasError);
-
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className={cn("flex flex-col gap-6", className)}
-      {...props}
-    >
-      <div className="flex flex-col items-center gap-2 text-center">
-        <p className="text-2xl font-bold">Login to your account</p>
+    <Form {...loginForm}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={cn("flex flex-col gap-6", className)}
+        {...props}
+      >
+        <div className="flex flex-col items-center gap-2 text-center">
+          <p className="text-2xl font-bold">Login to your account</p>
 
-        <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
-        </p>
-      </div>
+          <p className="text-muted-foreground text-sm text-balance">
+            Enter your email below to login to your account
+          </p>
+        </div>
 
-      {renderError && (
-        <InputPopupMessage message={renderError.message} isError={true} />
-      )}
-      {apiError && <InputPopupMessage message={apiError} isError={true} />}
-
-      <div className="grid gap-6">
-        <TextInputWithLabel
-          label="email"
-          id="emailInput"
-          type="email"
-          placeholder="m@example.com"
-          register={register}
-          isError={isEmailHasError ? true : false}
+        {/* Email Input */}
+        <FormField
+          control={control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  id="emailInput"
+                  type="email"
+                  placeholder="m@example.com"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
 
-        <div className="grid gap-1">
-          <TextInputWithLabel
-            label="password"
-            id="passwordInput"
-            type="password"
-            register={register}
-            isError={isPassHasError ? true : false}
-          />
+        {/* Password */}
+        <FormField
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex justify-between">
+                <FormLabel>Password</FormLabel>
 
-          <div className="flex justify-end">
-            <p
-              className="text-sm hover:text-primary transition cursor-pointer"
-              onClick={() => navigateTo("/forgot-password")}
-            >
-              Forgot your password?
-            </p>
-          </div>
-        </div>
+                <p
+                  className="text-sm underline-offset-4 hover:underline cursor-pointer"
+                  onClick={() => navigateTo("/forgot-password")}
+                >
+                  Forgot your password?
+                </p>
+              </div>
+
+              <FormControl>
+                <Input id="passwordInput" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
         {/* Submit Button */}
         <Button
@@ -83,46 +110,22 @@ export function LoginForm({ className, ...props }) {
             "Login"
           )}
         </Button>
-      </div>
 
-      <p className="text-center text-sm">
-        Don't have an account?{" "}
-        <span
-          className="underline underline-offset-4 hover:text-primary transition cursor-pointer"
-          onClick={() => navigateTo("/signup")}
-        >
-          Sign up
-        </span>
-      </p>
-    </form>
+        <p className="text-center text-sm">
+          Don't have an account?{" "}
+          <span
+            className="underline underline-offset-4 hover:opacity-80 transition cursor-pointer"
+            onClick={() => navigateTo("/signup")}
+          >
+            Sign up
+          </span>
+        </p>
+
+        {apiError && <InputPopupMessage message={apiError} isError={true} />}
+      </form>
+    </Form>
   );
 }
-
-const TextInputWithLabel = ({
-  id,
-  type,
-  placeholder,
-  // for react-hook-form
-  label,
-  register,
-  // for design
-  isError,
-}) => {
-  return (
-    <label className="grid gap-3">
-      <p className="text-sm capitalize font-medium">{label}</p>
-
-      <input
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        className={` rounded-lg text-sm p-2 bg-white w-full border placeholder:text-muted-foreground text-black
-            ${isError ? "border-2 border-red-500" : ""}`}
-        {...register(label)}
-      />
-    </label>
-  );
-};
 
 const InputPopupMessage = ({ message, isError }) => {
   return (
